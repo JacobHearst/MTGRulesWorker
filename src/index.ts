3ffd3200cccaf472eca201d87641fe0acf50e857
Jacob Hearst <jacob.hearst@jamf.com>
2022-07-05 09:12:45 -0500

    main Update rules path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { getRulesObject } from "./RulesParser"
 
 export default {
     async fetch(request: Request): Promise<Response> {
-        if (request.url.includes("/v1/mtg/rules")) {
+        if (request.url.includes("/v1/rules")) {
             const rules = await getRulesObject()
             return new Response(JSON.stringify(rules), { headers: { "Content-Type": "application/json" } })
         }
